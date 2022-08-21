1c8c4cadd3fb61bb3658f400a4e58597cb942dfc
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-22 00:52:05 +0300

    backend and frontend merged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState , useEffect } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+
+
 function App() {
+
+  const [restaurants, setRestaurants] = useState(null);
+
+  const fetchData = async () => {
+    const response = await axios.get('http://localhost:8000/restaurants');
+    setRestaurants(response.data.data)
+  }
+
+  useEffect(() => {
+    fetchData();
+  } , [])
+
+  console.log(restaurants)
+
   return (
     <div className="App">
       Hello world
