18f0417265ce85744aff827d6acd502e4a87fd1e
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-23 15:13:57 +0300

    Added delete and added restaurant in react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 import axios from 'axios';
@@ -10,6 +10,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import SearchBar from './components/SearchBar';
 import RestaurantList from './components/RestaurantList';
+import AddRestaurant from './components/AddRestaurant';
 
 
 
@@ -19,11 +20,13 @@ function App() {
   const [restaurants, setRestaurants] = useState([]);
   const [search, setSearch] = useState(null)
   const [map, setMap] = useState(new Map())
-  const [keys , setKeys] = useState([])
+  const [keys, setKeys] = useState([])
+
+  console.log(restaurants)
 
   const fetchData = async () => {
     const restaurantsData = await axios.get('http://localhost:8000/restaurants')
-    
+
 
     const data = Object.keys(restaurantsData.data.data).map(restaurant => restaurantsData.data.data[restaurant])
     const keys = Object.keys(restaurantsData.data.data)
@@ -34,7 +37,7 @@ function App() {
     for (let i = 0; i < keys.length; i++) {
       setMap(map.set(keys[i], data[i]))
     }
-    
+
   }
 
   useEffect(() => {
@@ -42,42 +45,68 @@ function App() {
     setSearch(" ")
   }, [])
 
+  const getMovies = async () => {  // Axios method Local Api   // axios dowload npm i axios
+
+    const restaurantsData = await axios.get('http://localhost:8000/restaurants')
+
+
+    const data = Object.keys(restaurantsData.data.data).map(restaurant => restaurantsData.data.data[restaurant])
+    const keys = Object.keys(restaurantsData.data.data)
+
+    console.log("Data")
+
+    setRestaurants(data)
+    setKeys(keys)
+
+    for (let i = 0; i < keys.length; i++) {
+      setMap(map.set(keys[i], data[i]))
+    }
+  }
+
   let filteredRestaurants = restaurants.filter(
     (restaurant) => {
       return restaurant.name.toLowerCase().indexOf(search.trim().toLowerCase()) !== -1
     }
-  ).sort(( a , b) => {
+  ).sort((a, b) => {
     return a.id < b.id ? 1 : a.id > b.id ? -1 : 0;
   }
   );
 
 
-  const addMovie = async (newRestaurant) => {
+  const addRestaurant = async (newRestaurant) => {
+
+    console.log(newRestaurant)
+
+    let highId = 0;
+
+    for (let i = 0; i < restaurants.length; i++) {
+      if (restaurants[i].id > highId) {
+        highId = restaurants[i].id;
+      }
+    }
+
+    newRestaurant.id = highId + 1;
 
     const baseUrl = 'http://localhost:8000/addRestaurant';
 
     await axios.post(baseUrl, newRestaurant)
 
+    getMovies()
 
-    // await axios.post(baseUrl, movie)
-    // this.setState(state => ({
-    //   movies: state.movies.concat([movie])
-    // }
-    // )
-    // )
-    // this.getMovies()
   }
-  
+
 
   const deleteRestaurant = async (deleteRestaurant) => {
+
+
 
     const id = keys[restaurants.indexOf(deleteRestaurant)]
 
     await axios.delete(`http://localhost:8000/deleteRestaurant/${id}`)
 
-
     const index = restaurants.indexOf(deleteRestaurant)
-    const newRestaurantList = restaurants.pop(deleteRestaurant)
+    
+    const newRestaurantList = restaurants.filter(restaurant => restaurant !== deleteRestaurant)
 
     setMap(map.delete(keys[index]))
     setKeys(keys.filter(k => k !== keys[index]))
@@ -90,60 +119,60 @@ function App() {
     setSearch(event.target.value)
   }
 
-  
+
 
 
   return (
 
     <BrowserRouter>
-        <Routes>
+      <Routes>
 
-          <Route path="/" element={
-            <div className="container">
+        <Route path="/" element={
+          <div className="container">
 
-              <div className="row">
-                <div className="col-lg-12">
-                  <br />
-                  <h1
-                  style = {{
-                    color : "red" ,
-                    fontSize : "50px" ,
-                    fontFamily : "cursive" ,
-                    align : "center" ,
-                    marginLeft : "350px" ,
-                    marginTop : "50px" ,
-                    marginBottom : "50px"
+            <div className="row">
+              <div className="col-lg-12">
+                <br />
+                <h1
+                  style={{
+                    color: "red",
+                    fontSize: "50px",
+                    fontFamily: "cursive",
+                    align: "center",
+                    marginLeft: "350px",
+                    marginTop: "50px",
+                    marginBottom: "50px"
                   }}
-                  >My Restaurant List</h1>
-                  <br />
-                  <SearchBar
-                    searchProp={searchRestaurant}
-                  />
-                </div>
+                >My Restaurant List</h1>
+                <br />
+                <SearchBar
+                  searchProp={searchRestaurant}
+                />
               </div>
-
-              <RestaurantList
-                restaurants={filteredRestaurants}
-                deleteRestaurantProp={deleteRestaurant}
-              />
-
             </div>
-          } />
 
-          {/* <Route path="/addRestaurant" element={
-            <AddMovie
-              onAddMovie={(movie) => { this.addMovie(movie) }}
+            <RestaurantList
+              restaurants={filteredRestaurants}
+              deleteRestaurantProp={deleteRestaurant}
             />
-          } />
 
-          <Route path="/editRestaurant/:id" element={
+          </div>
+        } />
+
+        <Route path="/addRestaurant" element={
+          <AddRestaurant
+            onAddRestaurant={(restaurant) => { addRestaurant(restaurant) }}
+          />
+        } />
+
+        {/* <Route path="/editRestaurant/:id" element={
             <EditMovie
               editMovie = {() => { this.editMovie() }}
             />
           } /> */}
 
-        </Routes>
-      </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/AddRestaurant.js
@@ -0,0 +1,158 @@
+import React from 'react'
+
+import { useNavigate } from "react-router-dom";
+
+import { Link } from "react-router-dom";
+
+import serialize from 'form-serialize';
+
+
+
+
+const AddRestaurant = (props) => {
+
+    const navigate = useNavigate();
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(e)
+        const formRestaurant = serialize(e.target, { hash: true });
+        
+        let menu = []
+        
+        formRestaurant.menu.trim().split(',').forEach(item => {
+            menu.push(item.trim())
+        } )
+
+        formRestaurant.menu = menu
+
+        const newRestaurant = {
+            name: formRestaurant.name,
+            country: formRestaurant.country,
+            description: formRestaurant.description,
+            image: formRestaurant.image,
+            rate : formRestaurant.rate,
+            menu : formRestaurant.menu ,
+            location: {
+                address: formRestaurant.address,
+                zipcode: formRestaurant.zipcode,
+                web: formRestaurant.web,
+            }
+        }
+
+        console.log(newRestaurant)
+
+        props.onAddRestaurant(newRestaurant);
+        navigate("/")
+    };
+
+    return (
+        <div className="container" style={
+            {
+                height: "100%",
+                margin: "0 auto",
+                verticalAlign: "middle",
+                align: "center",
+                horizontalAlign: "center"
+            }
+        }>
+
+            <form className="mt-5" onSubmit={handleFormSubmit}>
+                <div className="row">
+                    <div className='col-lg-1'>
+                        <Link
+                            to={{ pathname: "/" }}
+                            type="button"
+                            className="btn btn-nd btn-info"
+                            style={{ float: 'left' }}
+                        >
+                            Back
+                        </Link>
+                    </div>
+                    <div className='col-lg-11'>
+                        <input className="form-control" id="disabledInput" type="text" placeholder="Fill The Form To Add A Restaurant.." disabled />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-10">
+                        <label htmlFor="inputName">Name</label>
+                        <input type="text"
+                            className="form-control"
+                            name="name" />
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label htmlFor="inputRate">Rate</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="rate"
+                            placeholder='Exp : 9.7' />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-8">
+                        <label htmlFor="inputAddress">Address</label>
+                        <input type="text"
+                            className="form-control"
+                            name="address" />
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label htmlFor="inputCountry">Counrty</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="country" />
+                    </div>
+                    <div className="form-group col-md-2">
+                        <label htmlFor="inputZipCode">Zipcode</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="zipcode" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputWebSite">Web Site</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="web" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputImage">Image URL</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="image" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputDescripiton">Description</label>
+                        <textarea
+                            className="form-control"
+                            name="description" rows="1"></textarea>
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputMenu">Manu</label>
+                        <textarea
+                            className="form-control"
+                            name="menu" rows="1"
+                            placeholder="Exp :
+                            Fish,Soup , 
+                            "
+                        ></textarea>
+                    </div>
+                </div>
+                <input type="submit" className="btn btn-danger btn-block" value="Add Restaurant" />
+            </form>
+        </div>
+    )
+}
+
+export default AddRestaurant
