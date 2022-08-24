b1892c92194a146193a401d54c7f0455e8aaa6be
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-24 14:25:16 +0300

    Added country in card and styled card

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -50,27 +50,30 @@ const RestaurantList = (props) => {
                             />
                         </a>
                         <div className="card-body">
-                            <h4 className="card-title">
+                            <h4 className="card-title p-3 mb-2 bg-dark text-white" style={{textAlign : "center"}}>
                                 {restaurant.name}
                             </h4>
-                            <p className="card-text">
+                            <p className="card-text" style={{textAlign : "center"}}>
                                 {truncateOverview(restaurant.description, 200)}
                             </p>
 
+                            <div className="card-text text-danger" style={{textAlign : "center"}}>
+                                <h4>{restaurant.country}</h4>
+                            </div>    
 
-                            <div className="d-flex justify-content-between align-items-center">
-                                <p className="text-primary"> Address :   {restaurant.location.address} </p>
+                            <div className="d-flex justify-content-between align-items-center"  style={{textAlign : "center"}}>
+                                <p> <span className="text-primary "> <strong>Address :</strong>  </span> <small>{restaurant.location.address}</small>  </p>
                             </div>
 
 
 
                             <div>
-                                <h5 className="text-warning mx-auto" style={{ width: "70px" }} > Menu </h5>
+                                <h5 className="text-warning mx-auto" style={{ width: "70px" , textAlign : "center" }} > Menu </h5>
                                 <ul className="list-group">
                                     {restaurant.menu.map((menuItem, index) => (
                                         <div key={index}>
                                             <div >
-                                                <li className="list-group-item">{menuItem}</li>
+                                                <li className="list-group-item" style={{textAlign : "center"}}>{menuItem}</li>
                                             </div>
 
                                         </div>
@@ -78,7 +81,7 @@ const RestaurantList = (props) => {
                                 </ul>
                             </div>
                             <br></br>
-                            <div className="d-flex justify-content-between align-items-center">
+                            <div className="d-flex justify-content-between align-items-center" >
                                 <button type="button" onClick={
                                     (event) => {
                                         props.deleteRestaurantProp(restaurant);
