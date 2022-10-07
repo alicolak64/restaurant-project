4e3df6f61e77234e5f730b3a5c571b5c187ea3fb
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-10-07 18:00:55 +0300

    Added loading and error

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import RestaurantList from './components/RestaurantList';
 import AddRestaurant from './components/AddRestaurant';
 import EditRestaurant from './components/EditRestaurant';
 
+import Loading from './components/Loading/Loading';
+import Error from './components/Error/Error';
 
 
 
@@ -19,42 +21,61 @@ function App() {
   const [restaurants, setRestaurants] = useState([]);
   const [search, setSearch] = useState(null)
   const [map, setMap] = useState(new Map())
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-  const fetchData = async () => {
-    const restaurantsData = await axios.get('http://localhost:8000/restaurants')
-
-    const data = Object.keys(restaurantsData.data.data).map(restaurant => restaurantsData.data.data[restaurant])
-    const keys = Object.keys(restaurantsData.data.data)
-
-    setRestaurants(data)
-
-    for (let i = 0; i < keys.length; i++) {
-      setMap(map.set( keys[i] , data[i] ))
-    }
-
-  }
 
   useEffect(() => {
-    fetchData();
+
+    const fetchData = async () => {
+
+      await axios.get('http://localhost:8000/restaurants')
+        .then(response => {
+          const data = Object.keys(response.data.data).map(restaurant => response.data.data[restaurant])
+          const keys = Object.keys(response.data.data)
+          setRestaurants(data)
+
+          for (let i = 0; i < keys.length; i++) {
+            setMap(map.set(keys[i], data[i]))
+          }
+
+          setLoading(false)
+
+        })
+        .catch(error => {
+          setError(error)
+          setLoading(false)
+        })
+
+    }
+
+    fetchData()
     setSearch(" ")
-  }, [])
+
+  }, [map])
 
   const getRestaurants = async () => {  // Axios method Local Api   // axios dowload npm i axios
 
-    const restaurantsData = await axios.get('http://localhost:8000/restaurants')
+    await axios.get('http://localhost:8000/restaurants')
+      .then(response => {
+        const data = Object.keys(response.data.data).map(restaurant => response.data.data[restaurant])
+        const keys = Object.keys(response.data.data)
+        setRestaurants(data)
 
-    const data = Object.keys(restaurantsData.data.data).map(restaurant => restaurantsData.data.data[restaurant])
-    const keys = Object.keys(restaurantsData.data.data)
+        const newMap = map.clear()
 
-    setRestaurants(data)
+        setMap(newMap)
 
-    const newMap = map.clear()
+        for (let i = 0; i < keys.length; i++) {
+          setMap(map.set(keys[i], data[i]))
+        }
 
-    setMap(newMap)
+      })
+      .catch(error => {
+        setError(error)
+        setLoading(false)
+      })
 
-    for (let i = 0; i < keys.length; i++) {
-      setMap(map.set( keys[i] , data[i] ))
-    }
   }
 
   let filteredRestaurants = restaurants.filter(
@@ -86,13 +107,11 @@ function App() {
     const baseUrl = 'http://localhost:8000/addRestaurant';
 
     await axios.post(baseUrl, newRestaurant)
-    .then(response => {
-      getRestaurants()
-    }).catch(error => {
-      console.log(error)
-    })
-
-    
+      .then(response => {
+        getRestaurants()
+      }).catch(error => {
+        console.log(error)
+      })
 
   }
 
@@ -102,15 +121,15 @@ function App() {
     let key = ""
 
     for (const tempKey of map.keys()) {
-      if(map.get(tempKey) === deleteRestaurant) {
+      if (map.get(tempKey) === deleteRestaurant) {
         key = tempKey
       }
     }
 
     await axios.delete(`http://localhost:8000/deleteRestaurant/${key}`)
 
-    
-    let tempMap = map 
+
+    let tempMap = map
 
     tempMap.delete(key)
 
@@ -125,9 +144,6 @@ function App() {
   const searchRestaurant = (event) => {
     setSearch(event.target.value)
   }
-
-
-
 
   return (
 
@@ -158,11 +174,17 @@ function App() {
               </div>
             </div>
 
-            <RestaurantList
-              restaurants={filteredRestaurants}
-              map = {map}
-              deleteRestaurantProp={deleteRestaurant}
-            />
+            {
+              loading ?
+                <Loading /> :
+                error ?
+                  <Error message={error.message} /> :
+                  <RestaurantList
+                    restaurants={filteredRestaurants}
+                    map={map}
+                    deleteRestaurantProp={deleteRestaurant}
+                  />
+            }
 
           </div>
         } />
